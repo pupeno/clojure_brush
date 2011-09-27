777314ec4dc537f66690e9c9f079f003f442f177
J. Pablo Fernández <pupeno@pupeno.com>
2011-09-27 12:00:10 +0200

    Have a coding style more similar to the mainstream one.

--- shBrushClojure.js
+++ shBrushClojure.js
@@ -1,29 +1,39 @@
 // Copyright © 2009 Travis Whitton
 // Copyright © 2011 J. Pablo Fernández
 
-SyntaxHighlighter.brushes.Clojure = function() {
-  var funcs = ":arglists :doc :file :line :macro :name :ns :private :tag :test new alias alter " +
-              "and apply assert class cond conj count def defmacro defn defstruct deref do "     +
-              "doall dorun doseq dosync eval filter finally find first fn gen-class gensym if "  +
-              "import inc keys let list loop map ns or print println quote rand recur reduce "   +
-              "ref repeat require rest send seq set sort str struct sync take test throw "       +
-              "trampoline try type use var vec when while";
+;(function() {
+    // CommonJS
+    typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
 
-  this.regexList = [
-          // Explicitly match entities to avoid them matching a comment. https://github.com/alexgorbatchev/SyntaxHighlighter/issues/41
-          { regex: new RegExp("&", "gm"),                                 css: "keyword"},
-          { regex: new RegExp("&amp;", "gm"),                             css: "keyword"},
-          { regex: new RegExp("&gt;", "gm"),                              css: "keyword"},
-          { regex: new RegExp("&lt;", "gm"),                              css: "keyword"},
+    function Brush() {
+      var funcs = ":arglists :doc :file :line :macro :name :ns :private :tag :test new alias alter " +
+                  "and apply assert class cond conj count def defmacro defn defstruct deref do "     +
+                  "doall dorun doseq dosync eval filter finally find first fn gen-class gensym if "  +
+                  "import inc keys let list loop map ns or print println quote rand recur reduce "   +
+                  "ref repeat require rest send seq set sort str struct sync take test throw "       +
+                  "trampoline try type use var vec when while";
 
-          { regex: new RegExp(";.*$", "gm"),                               css: "comments" },
-          { regex: SyntaxHighlighter.regexLib.multiLineDoubleQuotedString, css: "string" },
-          { regex: /\(|\)|\[|\]/g,                                         css: "keyword" },
-          { regex: /'[a-z][A-Za-z0-9_\-]*/g,                               css: "constants" }, // symbols
-          { regex: /:[a-z][A-Za-z0-9_\-]*/g,                               css: "color1" }, // keywords
-          { regex: new RegExp(this.getKeywords(funcs), "gmi"),             css: "functions" }
-      ];
-}
+      this.regexList = [
+              // Explicitly match entities to avoid them matching a comment. https://github.com/alexgorbatchev/SyntaxHighlighter/issues/41
+              { regex: new RegExp("&", "gm"),                                 css: "keyword"},
+              { regex: new RegExp("&amp;", "gm"),                             css: "keyword"},
+              { regex: new RegExp("&gt;", "gm"),                              css: "keyword"},
+              { regex: new RegExp("&lt;", "gm"),                              css: "keyword"},
 
-SyntaxHighlighter.brushes.Clojure.prototype     = new SyntaxHighlighter.Highlighter(); 
-SyntaxHighlighter.brushes.Clojure.aliases       = ["clojure", "Clojure", "clj"];
+              { regex: new RegExp(";.*$", "gm"),                               css: "comments" },
+              { regex: SyntaxHighlighter.regexLib.multiLineDoubleQuotedString, css: "string" },
+              { regex: /\(|\)|\[|\]/g,                                         css: "keyword" },
+              { regex: /'[a-z][A-Za-z0-9_\-]*/g,                               css: "constants" }, // symbols
+              { regex: /:[a-z][A-Za-z0-9_\-]*/g,                               css: "color1" }, // keywords
+              { regex: new RegExp(this.getKeywords(funcs), "gmi"),             css: "functions" }
+              ];
+    }
+
+    Brush.prototype	= new SyntaxHighlighter.Highlighter();
+    Brush.aliases	= ["clojure", "Clojure", "clj"];
+
+    SyntaxHighlighter.brushes.Clojure = Brush;
+
+    // CommonJS
+    typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
+})();
