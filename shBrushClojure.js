41341d9212fd2c6264f7b17ee58959492a76c8b8
J. Pablo Fernández <pupeno@pupeno.com>
2011-09-27 11:13:09 +0200

    Fix the quote I broke.

--- shBrushClojure.js
+++ shBrushClojure.js
@@ -13,7 +13,7 @@ SyntaxHighlighter.brushes.Clojure = function() {
           { regex: new RegExp(";.*$", "gm"),                               css: "comments" },
           { regex: SyntaxHighlighter.regexLib.multiLineDoubleQuotedString, css: "string" },
           { regex: /\[|\]/g,                                               css: "keyword" },
-          { regex: /"[a-z][A-Za-z0-9_\-]*/g,                               css: "color1" }, // symbols
+          { regex: /'[a-z][A-Za-z0-9_\-]*/g,                               css: "color1" }, // symbols
           { regex: /:[a-z][A-Za-z0-9_\-]*/g,                               css: "color2" }, // keywords
           { regex: new RegExp(this.getKeywords(funcs), "gmi"),             css: "functions" }
       ];
