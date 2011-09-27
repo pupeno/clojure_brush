e4fe94cd1a102d380b788af2f5984b207385ae54
J. Pablo Fernández <pupeno@pupeno.com>
2011-09-27 10:54:40 +0200

    Added copyright notice and removed contribution comment.

--- shBrushClojure.js
+++ shBrushClojure.js
@@ -1,7 +1,7 @@
+// Copyright © 2009 Travis Whitton
+
 SyntaxHighlighter.brushes.Clojure = function()
 {
-  // Contributed by Travis Whitton
-
   var funcs = ':arglists :doc :file :line :macro :name :ns :private :tag :test new alias alter ' +
               'and apply assert class cond conj count def defmacro defn defstruct deref do '     +
               'doall dorun doseq dosync eval filter finally find first fn gen-class gensym if '  +
