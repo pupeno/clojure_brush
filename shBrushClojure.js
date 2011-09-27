cd84f6c33934a030312a52245be28bd2172e8039
J. Pablo Fernández <pupeno@pupeno.com>
2011-09-27 12:26:51 +0200

    Do not use a list of functions, it's impossible to keep up to date.
    Also… there's no difference between functions or macros defined by Clojure or third parties.

--- shBrushClojure.js
+++ shBrushClojure.js
@@ -6,27 +6,24 @@
     typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
 
     function Brush() {
-      var funcs = ":arglists :doc :file :line :macro :name :ns :private :tag :test new alias alter " +
-                  "and apply assert class cond conj count def defmacro defn defstruct deref do "     +
-                  "doall dorun doseq dosync eval filter finally find first fn gen-class gensym if "  +
-                  "import inc keys let list loop map ns or print println quote rand recur reduce "   +
-                  "ref repeat require rest send seq set sort str struct sync take test throw "       +
-                  "trampoline try type use var vec when while";
+        function process(match, regexInfo) {
+            return [new SyntaxHighlighter.Match(match[1], match.index + 1, "functions")];
+        }
 
-      this.regexList = [
-              // Explicitly match entities to avoid them matching a comment. https://github.com/alexgorbatchev/SyntaxHighlighter/issues/41
-              { regex: new RegExp("&", "gm"),                                 css: "keyword"},
-              { regex: new RegExp("&amp;", "gm"),                             css: "keyword"},
-              { regex: new RegExp("&gt;", "gm"),                              css: "keyword"},
-              { regex: new RegExp("&lt;", "gm"),                              css: "keyword"},
+        this.regexList = [
+            // Explicitly match entities to avoid them matching a comment. https://github.com/alexgorbatchev/SyntaxHighlighter/issues/41
+            { regex: new RegExp("&", "gm"),                                 css: "keyword"},
+            { regex: new RegExp("&amp;", "gm"),                             css: "keyword"},
+            { regex: new RegExp("&gt;", "gm"),                              css: "keyword"},
+            { regex: new RegExp("&lt;", "gm"),                              css: "keyword"},
 
-              { regex: new RegExp(";.*$", "gm"),                               css: "comments" },
-              { regex: SyntaxHighlighter.regexLib.multiLineDoubleQuotedString, css: "string" },
-              { regex: /\(|\)|\[|\]/g,                                         css: "keyword" },
-              { regex: /'[a-z][A-Za-z0-9_\-]*/g,                               css: "constants" }, // symbols
-              { regex: /:[a-z][A-Za-z0-9_\-]*/g,                               css: "color1" }, // keywords
-              { regex: new RegExp(this.getKeywords(funcs), "gmi"),             css: "functions" }
-              ];
+            { regex: new RegExp(";.*$", "gm"),                               css: "comments" },
+            { regex: SyntaxHighlighter.regexLib.multiLineDoubleQuotedString, css: "string" },
+            { regex: /\(|\)|\[|\]/g,                                         css: "keyword" },
+            { regex: /'[a-z][A-Za-z0-9_\-]*/g,                               css: "constants" }, // symbols
+            { regex: /:[a-z][A-Za-z0-9_\-]*/g,                               css: "color1" }, // keywords
+            { regex: /\(([a-z][A-Za-z0-9_\-]*)/gm,             func: process },
+        ];
     }
 
     Brush.prototype	= new SyntaxHighlighter.Highlighter();
